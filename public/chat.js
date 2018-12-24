5aac21a83902d8b57d953d5e44ef0b916ffc2676
Prince Donkor <prince.donkor74@gmail.com>
2018-12-23 19:09:58 -0600

    now able to broadcast a message

--- public/chat.js
+++ public/chat.js
@@ -6,7 +6,7 @@ var message = document.getElementById('message');
     handle = document.getElementById('handle');
     btn = document.getElementById('send');
     output = document.getElementById('output');
-
+    feedback = document.getElementById('feedback');
 
 
 //Emit event
@@ -18,8 +18,18 @@ btn.addEventListener('click', function(){
     })
 })
 
+message.addEventListener('keypress', function(){
+    socket.emit("typing", handle.value)
+})
+
 //Listen for events 
 
 socket.on('chat', function(data){
+    feedback.innerHTML = ""
     output.innerHTML += "<p><strong>" + data.handle + ": </strong> " + data.message + "</p>"
+
+})
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' </em> is typing a message...</p>'
 })
